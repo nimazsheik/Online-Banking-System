3d0a86f5792ba05f6c514d165159afcfae9af580
yasanthasampath <haysweerasinghe@gmail.com>
2015-02-13 12:23:28 +0530

    add new text
    added text file

--- Online_Banking_Application/src/java/model/User.java
+++ Online_Banking_Application/src/java/model/User.java
@@ -114,7 +114,49 @@ public class User {
         this.valid = valid;
     }
 
-
+public static User checkUserLogin(User user){
+        
+            PreparedStatement stmnt= null;
+            ResultSet rs = null;
+           // User user = new User();
+          try {    
+            //setUserName ( uname);
+           // setPassword ( pwd);
+            
+         //   String checkQuery = "SELECT * FROM users WHERE user_name = '"+getUserName()+"' AND password = '"+getPassword() +"'";
+            String checkQuery = "SELECT * FROM users WHERE user_name = ? AND password = ?";
+            // now check the database if user exists, if exists return his id number
+            DatabaseConnection databaseConnect = new DatabaseConnection();
+            
+            
+            stmnt = databaseConnect.getConnection().prepareStatement(checkQuery);
+            stmnt.setString(1, user.getUserName());
+            stmnt.setString(2, user.getPassword());
+            rs = stmnt.executeQuery();
+            
+            //boolean loggedIn = rs.next();
+           
+            if(rs.next()){
+                user.setUserID(rs.getInt("user_id"));
+                user.setFirstName(rs.getString("first_name"));
+                user.setLastName(rs.getString("last_name"));
+                user.setDateOfBirth(rs.getDate("date_of_birth"));
+                user.setGender(rs.getString("gender"));
+                user.setEmail(rs.getString("email"));
+                user.setUserName(rs.getString("user_name"));
+                user.setPassword(rs.getString("password"));
+                user.setValid(true);
+            }else{
+                System.out.println("fail login");
+                user.setValid(false);
+            }
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(User.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return user;
+    }
    
     
 }
